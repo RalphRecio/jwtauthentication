93a5f4cf63c2ccfa1bae6c505e4ca39362a6d50a
RalphRecio <jralphrecio@gmail.com>
2023-08-20 20:49:35 +0800

    Add project files.

---
+++ Controllers/AuthenticateController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace jwtauthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Get([FromBody] LoginModel loginModel)
+        {
+            if (loginModel.username == null || loginModel.password == null)
+            {
+                return BadRequest("Username and password are required!");
+            }
+
+
+
+            if(loginModel.username != "Admin" && loginModel.password != "Admin") 
+                return Unauthorized();
+       
+
+            var token = GenerateToken(loginModel.username);
+
+
+            return StatusCode(200, new { message = "Success", token = token });
+        }
+
+        private string GenerateToken(string userName)
+        {
+            try
+            {
+
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,userName)
+                };
+
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(
+                        _configuration["ApiKey"]
+                    ));
+
+
+                var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+                var token = new JwtSecurityToken(
+                        claims: claims,
+                        expires: DateTime.Now.AddDays(1),
+
+                        signingCredentials: cred,
+                        issuer: _configuration["Jwt:Issuer"]
+                    );
+
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwt = tokenHandler.WriteToken(token);
+
+                return jwt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+
+
+    public class LoginModel
+    {
+
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["ApiKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
